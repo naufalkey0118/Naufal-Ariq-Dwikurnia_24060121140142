a980b3d0b73fecdf8417ff78296c055bec77c6af
naufalkey0118 <100133982+naufalkey0118@users.noreply.github.com>
2023-03-09 14:04:17 +0700

    Add files via upload

---
+++ praktikum 3/BANGUN DATAR/Segitiga.java
@@ -0,0 +1,35 @@
+/**
+* File : Segitiga.java 08/03/2023
+* Penulis : Ahmad Alvin Griffin
+* Deskripsi : driver class untuk poligon dan segitiga
+*/
+
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon {
+
+    private double alas;
+    private double tinggi;
+    private double sisiMiring;
+
+    public Segitiga(double alas, double tinggi, int jumlahSisi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+        this.sisiMiring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+		this.jumlahSisi = jumlahSisi;
+    }
+
+    public double hitungLuas() {
+        return 0.5 * alas * tinggi;
+    }
+
+    public double hitungKeliling() {
+        return alas + tinggi + sisiMiring;
+    }
+	public void printInfo(){
+		System.out.println("Bangun segitiga bersisi "+this.getJumlahSisi());
+	}
+	
+}
