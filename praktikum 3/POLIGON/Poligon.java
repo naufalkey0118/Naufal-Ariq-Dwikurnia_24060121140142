dbca2387c72885f9908b608bf46d90f067ea0f16
naufalkey0118 <100133982+naufalkey0118@users.noreply.github.com>
2023-03-09 13:55:08 +0700

    Add files via upload

---
+++ praktikum 3/POLIGON/Poligon.java
@@ -0,0 +1,13 @@
+/* File : Poligon.java 08/03/2023
+* Penulis : Ahmad Alvin Griffin
+* Deskripsi : representasi dasar dara objek poligon (segi banyak)
+*/
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
