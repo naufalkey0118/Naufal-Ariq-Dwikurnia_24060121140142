f76f9a692a33aaaebf7402337940a00980cba3e0
naufalkey0118 <100133982+naufalkey0118@users.noreply.github.com>
2023-05-08 01:09:19 +0700

    Create Payroll.java

---
+++ praktikum 7/PolimerfismeInclusion(pegawaidangaji)/Payroll.java
@@ -0,0 +1,12 @@
+/**
+ * File      : Payroll.java		03/05/23
+ * Penulis   : Naufal Ariq Dwikurnia/ 24060121140142
+ * Deskripsi : Payroll.java adalah kelas yang berisi metode mencetak gaji pegawai
+ *
+ */
+
+public class Payroll {
+    public void cetakGaji(Pegawai p) {
+        p.tampilData();
+    }
+}
