ff0223e2038d5ecdbea69a50a57d2f2f5577260f
naufalkey0118 <100133982+naufalkey0118@users.noreply.github.com>
2023-06-06 19:59:43 +0700

    Create PersonDAO.java

---
+++ Pertemuan 9/PersonDAO.java
@@ -0,0 +1,10 @@
+/**
+ * File      : PersonDAO.java		31/05/23
+ * Penulis   : Naufal Ariq Dwikurnia / 24060121140142
+ * Deskripsi : interface untuk person access object
+ *
+ */
+
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
